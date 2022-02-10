3b6ddbe5f705169dc8b89a3df4bf39127d92c799
Oleg Peunov <olegpeunov@yandex.ru>
2022-02-10 21:47:38 +0300

    few console errors fixed

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -11,7 +11,7 @@ function Table(props) {
           return(
             <div onClick={props.setttingElementFunction} className="component__radio-element"  key={el.entryPointDocument}  >
               <input  type="radio" checked={el.entryPointDocument===props.chosenEl? true : false} id={el.entryPointDocument} name="contact" onChange={e => {}}/>
-              <label id={el.entryPointDocument} data-title={el.description} className="component__radio-text" for={el.entryPointDocument}>{el.description}</label>
+              <label id={el.entryPointDocument} data-title={el.description} className="component__radio-text" htmlFor={el.entryPointDocument}>{el.description}</label>
             </div>
           )
         })}
