c555086b3e82d344648bd773f54aadf5649837f8
Oleg Peunov <olegpeunov@yandex.ru>
2022-02-10 21:35:54 +0300

    Adding a new value component developed

---
+++ src/components/NewValue.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function NewValue(props) {
+
+  const inputRef = React.useRef()
+  const [newVal, setNewVal] = React.useState({})
+
+  function addNewValue(evt){
+    setNewVal({...newVal, "description": evt.target.value, "entryPointDocument": `evt.target.value ${new Date()}`})
+  
+  }
+
+  function submitNewVal(evt){
+    evt.preventDefault();
+    inputRef.current.value=''
+    props.updaveValues(newVal)
+  }
+
+
+  
+  return (
+    <form className="component" onSubmit={submitNewVal}>
+      <h2 className="component__name">Добавьте нове значение</h2>
+      <input type="text" minLength="2" ref={inputRef} onChange={addNewValue}/>
+
+ 
+    </form>
+  );
+}
+
+export default NewValue;
